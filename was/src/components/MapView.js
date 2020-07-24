0de8aecdcaa549c867967e649945ef5c64ecaf1e
unknown <arun10072000@gmail.com>
2020-07-24 21:50:07 +0530

    Map Completed

--- was/src/components/MapView.js
+++ was/src/components/MapView.js
@@ -1,42 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import * as waterData from "./../data/waterbodyLocation.json";
 import "./styles/MapCSS.css";
+import _url from './../URL'
+const axios = require('axios')
 
-
-export default function MapView() {
-    const [activeWB, setActiveWB] = React.useState(null);
-    return (
-        <Map center={[9.925201, 78.119774]} zoom={12}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {waterData.features.map(wb => (
-                < Marker key={wb.properties.ID} position={[wb.geometry.coordinates[0], wb.geometry.coordinates[1]]}
-                    onClick={() => {
-                        setActiveWB(wb);
-                    }}
+export default class MapView extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            activeWB: null,
+            setActiveWB: null,
+            waterDataArray: []
+        }
+    }
+    componentWillMount() {
+        this.getWaterBodies()
+    }
+    setActiveWB(wb) {
+        this.setState({ activeWB: wb })
+    }
+    getWaterBodies() {
+        const URL = _url + "/waterinfo";
+        axios.get(URL).then(res => {
+            this.setState({ waterDataArray: res.data })
+            console.log(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    render() {
+        return (
+            <Map center={[9.925201, 78.119774]} zoom={12} >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-            ))}
-            {activeWB && (
-                <Popup
-                    position={[
-                        activeWB.geometry.coordinates[0],
-                        activeWB.geometry.coordinates[1]
-                    ]}
-                    onClose={() => {
-                        setActiveWB(null);
-                    }}
-                >
-                    <div>
-                        <h2>{activeWB.properties.NAME}</h2>
-                        <p>{activeWB.properties.ADDRESS}</p>
-                        <p>{activeWB.properties.CAPACITY}:{activeWB.properties.AVAILABILITY}</p>
-                    </div>
-                </Popup>
-            )}
-        </Map>
-    )
+                {this.state.waterDataArray.map(wb => (
+                    < Marker key={wb.unique_id} position={[wb.latitude, wb.longitude]}
+                        onClick={() => {
+                            this.setActiveWB(wb);
+                        }}
+                    />
+                ))}
+                {
+                    this.state.activeWB && (
+                        <Popup
+                            position={[
+                                this.state.activeWB.latitude,
+                                this.state.activeWB.longitude
+                            ]}
+                            onClose={() => {
+                                this.setActiveWB(null);
+                            }}
+                        >
+                            <div>
+                                <h2>{this.state.activeWB.tank_name}</h2>
+                                <p>{this.state.activeWB.village}</p>
+                                <p>{this.state.activeWB.max_capacity}:{this.state.activeWB.available_capacitys}</p>
+                            </div>
+                        </Popup>
+                    )
+                }
+            </Map >
+        )
+    }
+
 }
